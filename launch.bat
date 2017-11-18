cabf48827dd919a3b6770a14cecf4d181eae3a7f
victorsempere <victor.sempere1@gmail.com>
2017-11-18 12:37:22 +0100

    Partially optimized dockerfile
    Reduced 500MB. Pending to release at least 500M more. And maybe much
    more if we use Alpine distribution

--- launch.bat
+++ launch.bat
@@ -1,1 +1,1 @@
-docker run -ti --rm -v %2:/root/repo -v %3:/root/eclipse-workspace -e DISPLAY=%1:0.0 eclipse /bin/bash
+docker run -ti --rm -v %2:/root/repo -v %3:/root/eclipse-workspace -e DISPLAY=%1:0.0 eclipse
